e43ed20ee0c62d437aebde58474c3a0fd81b2045
Ryan Skotnicki <ryanskotnicki@gmail.com>
2020-05-04 23:06:53 -0600

    DB config and startup mods

---
+++ src/config/database-config.interface.ts
@@ -0,0 +1,9 @@
+export interface DatabaseConfig {
+    type: 'postgres';
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+    synchronize: boolean;
+}
