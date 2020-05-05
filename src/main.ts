b502a46ee6fe9275d51b802ac4767dfd413bbc4f
Ryan Skotnicki <ryanskotnicki@gmail.com>
2020-05-04 23:24:53 -0600

    Add global prefix

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,15 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   app.enableCors();
-  // } else {
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } 
+  //else {
   //   app.enableCors({ origin: serverConfig.origin });
   //   logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
   // }
 
+  app.setGlobalPrefix('v1/api');
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
