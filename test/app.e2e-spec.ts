dd600852536c7d21a93b7720bc66147597c82f92
Ryan Skotnicki <ryanskotnicki@gmail.com>
2020-05-06 20:31:50 -0600

    Enable e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,51 +5,51 @@ import { AppModule } from './../src/app.module';
 import { ConfigService } from '../src/config/config.service';
 import { AppService } from '../src/app.service';
 
-// describe('AppController (e2e)', () => {
-//   let prefix = '';
-//   // const hello = 'Hello World!';
-//   const hello = ['Hello World!'];
-//   const result = {
-//     getHello: hello,
-//   };
-//   let app: INestApplication;
-//   const appService = {
-//     getHello: () => result.getHello,
-//   };
-
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     })
-//       .overrideProvider(AppService)
-//       .useValue(appService)
-//       .compile();
-
-//     app = moduleFixture.createNestApplication();
-//     const configService: ConfigService = app.get(ConfigService);
-//     prefix = configService.get('URL_PREFIX');
-//     app.setGlobalPrefix(prefix);
-//     await app.init();
-//   });
-
-//   it('/${prefix} (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get(`/${prefix}/`)
-//       .expect(200)
-//       // .expect(JSON.stringify(result.getHello));
-//       .expect(result.getHello);
-//   });
-
-//   afterAll(async () => {
-//     await app.close();
-//   })
-// });
-
 describe('AppController (e2e)', () => {
+  let prefix = '';
+  // const hello = 'Hello World!';
+  const hello = ['Hello World!'];
+  const result = {
+    getHello: hello,
+  };
+  let app: INestApplication;
+  const appService = {
+    getHello: () => result.getHello,
+  };
 
-  it(`/users (GET)`, () => {
-    const word = 'hello';
-    expect(word).toEqual('hello');
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(AppService)
+      .useValue(appService)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    const configService: ConfigService = app.get(ConfigService);
+    prefix = configService.get('URL_PREFIX');
+    app.setGlobalPrefix(prefix);
+    await app.init();
   });
 
+  it('/${prefix} (GET)', () => {
+    return request(app.getHttpServer())
+      .get(`/${prefix}/`)
+      .expect(200)
+      // .expect(JSON.stringify(result.getHello));
+      .expect(result.getHello);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  })
 });
+
+// describe('AppController (e2e)', () => {
+
+//   it(`/users (GET)`, () => {
+//     const word = 'hello';
+//     expect(word).toEqual('hello');
+//   });
+
+// });
